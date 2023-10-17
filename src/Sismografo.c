6f1493945251982c0e4415c75267b3cc0ce3569b
Bryan Salazar <BRYAN.SALAZARMARTINEZ@ucr.ac.cr>
2023-10-16 21:56:52 -0600

    version 2

--- src/Sismografo.c
+++ src/Sismografo.c
@@ -1,11 +1,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <libopencm3/stm32/adc.h>
 #include "clock.h"
 #include "console.h"
 #include "sdram.h"
 #include "lcd-spi.h"
 #include "gfx.h"
+#include "adc.c"
 //Global Variables
 uint8_t com_en;
 uint16_t battery;
@@ -15,8 +18,14 @@ int main(void)
 {
 	clock_setup();
 	console_setup(115200);
+	adc_setup();
 	sdram_init();
 	lcd_spi_init();
+	com_en = 0;
+
+
+
+	//Welcome 
 	gfx_init(lcd_draw_pixel, 240, 320);
 	gfx_fillScreen(LCD_BLACK);
 	gfx_fillRoundRect(10, 10, 220, 220, 5, LCD_WHITE);
@@ -36,11 +45,24 @@ int main(void)
     int16_t gyr_y;
     int16_t gyr_z;
 	char int_to_str[7];
-	char lcd_out[3];  
+	char lcd_out[3]; 
+
+
 	while (1) {
         gyr_x = 0;
         gyr_y = 0;
         gyr_z = 0;
+
+		if (gpio_get(GPIOA, GPIO0)) {
+			if(com_en){
+				com_en = 0;
+			}
+			else{
+				com_en = 1;
+			}
+		}
+
+
 		gfx_setTextColor(LCD_CYAN, LCD_BLACK);
 		gfx_setTextSize(3);
 		gfx_fillScreen(LCD_BLACK);	
@@ -59,16 +81,44 @@ int main(void)
 		strcat(lcd_out, int_to_str);
 		gfx_setCursor(15, 144);
 		gfx_puts(lcd_out);
+		gfx_setTextSize(2);
 		battery = 0;
 		sprintf(int_to_str, "%d", battery);
-		gfx_setCursor(15, 198);
-		gfx_puts("Batt: ");
-		gfx_setCursor(130, 232);
+		gfx_setCursor(15, 250);
+		gfx_puts("Bateria: ");
+		gfx_setCursor(145, 250);
 		gfx_puts(int_to_str);
-		gfx_setCursor(160, 232);
-		gfx_puts("V");
+		gfx_setCursor(165, 250);
+		gfx_puts("%");
 		lcd_show_frame();
+
+
+
+		battery = adc_update();
+		if(battery <= 7){
+			batt_alarm = 1;
+			//gpio_set(GPIOG, GPIO14);
+		}
+		else{
+			batt_alarm = 0;
+			//gpio_clear(GPIOG, GPIO14);
+		}
+		if(com_en){
+	    	print_decimal_xyz(gyr_x); console_puts(",");
+        	print_decimal_xyz(gyr_y); console_puts(",");
+        	print_decimal_xyz(gyr_z); console_puts(",");
+			print_decimal_xyz(batt_alarm); console_puts(",");
+			print_decimal_xyz(battery); console_puts("\n");
+
+		}
+		else{
+			//gpio_clear(GPIOG, GPIO13);
+		}
+
+
+
 		msleep(500);
+
 	}
 	return 0;
 }
